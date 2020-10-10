269a4111713a358820f84cd038d56b6625dcda1a
Fernando Henrique Fernandes <nandohfernandes@gmail.com>
2020-10-10 15:44:49 -0300

    feat: add pdffile model

--- convert_pdf/models.py
+++ convert_pdf/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 # Create your models here.
-class File(models.Model):
+class PdfFile(models.Model):
     file_name = models.CharField(max_length=500)
-    date = models.DateTimeField('date')
+    date = models.DateTimeField("date")
+    upload = models.FileField(upload_to='uploads/')
+
+    def __str__(self):
+        return self.file_name
